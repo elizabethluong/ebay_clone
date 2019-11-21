83809daa76e48797f3d598b49d88b0564746d99c
Elizabeth Luong <elizabeth@Belles-MacBook-Air.local>
2019-11-21 13:33:58 +0000

    render mockup ejs

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 
 app.use(express.static("views"));
 app.use(express.static("public"));
 app.use(express.urlencoded());
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => res.render("index.html"));
 
 app.post("/listing", (req, res) => {
-  const userInputName = req.body.name;
-  const userInputPrice = req.body.price;
-  const userInputDescription = req.body.description;
-  res.end();
-  console.log(userInputName);
+  res.render("mockup.ejs", {
+    userInputName: req.body.name,
+    userInputPrice: req.body.price,
+    userInputDescription: req.body.description
+  });
 });
 
+//   res.send(
+//     // instead res.render("index.html")
+//     `Thanks ${userInputName}, I got the form
+//         Thanks ${userInputPrice} this is an option
+//         Thanks ${userInputDescription} this is a message.`
+
+// fetch(userInputName + userInputPrice + userInputDescription).then(response => {
+//   res.render("mockup.html");
+// });
 app.listen(port, () => console.log(`Listening on port ${port}!`));
